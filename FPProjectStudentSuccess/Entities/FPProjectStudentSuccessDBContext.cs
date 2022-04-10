3e624589b7079caa5b0b01e773e1c6b1be19d919
Leonardo Ferreira <leeeo.ferreira@hotmail.com>
2022-04-10 14:21:13 -0700

    vai

--- FPProjectStudentSuccess/Entities/FPProjectStudentSuccessDBContext.cs
+++ FPProjectStudentSuccess/Entities/FPProjectStudentSuccessDBContext.cs
@@ -29,7 +29,6 @@ namespace FPProjectStudentSuccess.Entities
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
                 optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=FPProjectStudentSuccessDB;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False");
             }
         }

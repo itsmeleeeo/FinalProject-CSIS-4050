8fa43d33d1a5074830768c0bdd29d6206504a2e1
Henrique Barros <rodriguesdebarrosh@student.douglascollege.ca>
2022-04-10 14:16:13 -0700

    Blazor app and views

---
+++ FPProjectStudentSuccessBSA/Data/DBCRUD.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FPProjectStudentSuccessBSA.Data
+{
+    public class DBCRUD
+    {
+        public static SQLConnectionConfig _conn;
+        public SqlConnection connection;
+        public DBCRUD(SQLConnectionConfig conn)
+        {
+            _conn = conn;
+            connection = new SqlConnection(_conn.Value);
+        }
+        public async Task ChangeOperation(string SQLQuery)
+        {
+            connection.Open();
+            SqlCommand command = new SqlCommand(SQLQuery, connection);
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+        public DataTable ReadOperation(string SQLQuery)
+        {
+            SqlCommand command = new SqlCommand(SQLQuery, connection);
+            SqlDataAdapter da = new SqlDataAdapter(command);
+            DataTable table = new DataTable();
+            da.Fill(table);
+            return table;
+        }
+    }
+}
---
+++ FPProjectStudentSuccessBSA/Service/ProductService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using FPProjectStudentSuccessBSA.Data;
+using FPProjectStudentSuccessBSA.Models;
+using System.Data;
+using System.Text;
+using FPProjectStudentSuccess.Entities;
+
+namespace FPProjectStudentSuccessBSA.Service
+{
+    public class ProductService
+    {
+        public static SQLConnectionConfig _conn;
+        public SqlConnection connection;
+        public ProductService(SQLConnectionConfig conn)
+        {
+            _conn = conn;
+            connection = new SqlConnection(_conn.Value);
+        }
+        public async Task<List<Product>> GetProductsAsync()
+        {            
+            using (var ctx = new FPProjectStudentSuccessDBContext())
+            {
+                var result = ctx.Product;
+                return await Task.FromResult(result.ToList());
+            }
+        }
+        public async Task<Product> InsertProductAsync(Product product)
+        {
+            using (var ctx = new FPProjectStudentSuccessDBContext())
+            {
+                ctx.Product.Add(product);
+                await ctx.SaveChangesAsync();
+            }
+            return product;
+        }        
+    }
+}
